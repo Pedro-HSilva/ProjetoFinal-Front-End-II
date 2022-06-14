8cc29c80a1fb596d38433aaa8ba9e19da7a10add
Pedro-HSilva <p3drohdasilva@gmail.com>
2022-06-14 20:53:34 -0300

    First Commit

---
+++ src/cadastro.ts
@@ -0,0 +1,28 @@
+const botao: any = document.getElementById("criarContaDois");
+
+function validarCadastro() {
+  const nomeUsuario: any = document.getElementById("username");
+  const senhaUsuario: any = document.getElementById("password");
+  const senhaDois: any = document.getElementById("passwordrepeat");
+
+  if (!nomeUsuario.value || !senhaUsuario.value || !senhaDois.value) {
+    alert("Todos os campos são de preenchimento obrigatório");
+    return;
+  }
+
+  if (senhaUsuario.value !== senhaDois.value) {
+    alert("As senhas devem ser iguais");
+    return;
+  }
+
+  const userInfo = {
+    nome: nomeUsuario.value,
+    senha: senhaUsuario.value,
+    messages: [],
+  };
+
+  localStorage.setItem(userInfo.nome, JSON.stringify(userInfo));
+  window.location.href = "login.html";
+}
+
+botao.addEventListener("click", validarCadastro);
---
+++ src/login.ts
@@ -0,0 +1,28 @@
+const botaoEntrar: any = document.getElementById("entrar");
+
+function entrarLista() {
+  const nomeUsuario: any = document.getElementById("username");
+  const senhaUsuario: any = document.getElementById("password");
+  const usernameStorage: any = localStorage.getItem(nomeUsuario.value);
+  const usernameObjeto: any = JSON.parse(usernameStorage);
+
+  if (!nomeUsuario.value || !senhaUsuario.value) {
+    alert("Todos os campos são de preenchimento obrigatório");
+    return;
+  }
+
+  if (!usernameStorage || usernameObjeto.senha !== senhaUsuario.value) {
+    alert("Usuário ou senha não cadastrado!");
+    return;
+  }
+
+  if (
+    usernameObjeto.senha === senhaUsuario.value ||
+    usernameObjeto.nome === nomeUsuario.value
+  ) {
+    sessionStorage.setItem("usuarioLogado", usernameStorage);
+    window.location.href = "listaRecados.html";
+  }
+}
+
+botaoEntrar.addEventListener("click", entrarLista);
